5501393abb7cbb0c4b10933f98ca7f7aa5ce06e9
Samuel Bailey <samuel.bailey@cambridgeconsultants.com>
2017-11-30 10:47:50 +0000

    Created index.php which contains a very basic sign up UI

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Title of the document</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <form>
+        <input type="text" name="first" placeholder="Firstname"><br>
+        <input type="text" name="last" placeholder="Lastname"><br>
+        <input type="text" name="uid" placeholder="Username"><br>
+        <input type="password" name="pwd" placeholder="Password"><br>
+        <button type="submit">Sign Up</button>
+    </form>
+</body>
+</html>
